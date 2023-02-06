44cccbee0ae477fc55171e54c48bd0d28f70bdf0
Yilmaz Mustafa <dev@mail.be>
2023-02-06 23:44:23 +0100

    fixes on WooCommerce API

--- service-fooda/src/main/java/org/sibernetik/fooda/service/WooCommerceController.java
+++ service-fooda/src/main/java/org/sibernetik/fooda/service/WooCommerceController.java
@@ -1,70 +1,64 @@
 package org.sibernetik.fooda.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-import com.icoderman.woocommerce.oauth.*;
 import com.icoderman.woocommerce.*;
 
 @RestController
+@RequestMapping("")
 public class WooCommerceController {
 
-    private static final String CONSUMER_KEY = "ck_b969666524ce65b909b7cb9c3b3598f49a613ef8";
-    private static final String CONSUMER_SECRET = "cs_bb15fb7a9b1a84d69c1ae864588985aa2df66f68";
-    private static final String WC_URL = "http://localhost/index.php";
-
+    @Autowired
     private WooCommerce wooCommerce;
 
-    public WooCommerceController(WooCommerce wooCommerce){
-        wooCommerce = new WooCommerceAPI(new OAuthConfig(WC_URL, CONSUMER_KEY, CONSUMER_SECRET), ApiVersionType.V2);
-    }
-
-    @PostMapping("products/create")
-    public Map createProduct(Map<String, Object> productInfo) {
-        Map product = wooCommerce.create(EndpointBaseType.PRODUCTS.getValue(), productInfo);
+    @PostMapping("/products/create")
+    public Map<String, Object> createProduct(Map<String, Object> productInfo) {
+        Map<String, Object> product = (Map<String, Object>) wooCommerce.create(EndpointBaseType.PRODUCTS.getValue(), productInfo);
         return product;
     }
 
-    @GetMapping("products")
-    public Object getAllProducts() {
+    @GetMapping("/products")
+    public List<Map<String, Object>> getAllProducts() {
         Map<String, String> params = new HashMap<>();
         params.put("per_page", "100");
         params.put("offset", "0");
-        Object products = wooCommerce.getAll(EndpointBaseType.PRODUCTS.getValue(), params);
+        List<Map<String, Object>> products = (List<Map<String, Object>>) wooCommerce.getAll(EndpointBaseType.PRODUCTS.getValue(), params);
         return products;
     }
 
 
-    @GetMapping("products/{id}")
-    public Map getProductById(@PathVariable("id") Integer id) {
-        Map product = wooCommerce.get(EndpointBaseType.PRODUCTS.getValue(), id);
+    @GetMapping("/products/{id}")
+    public Map<String, Object> getProductById(@PathVariable("id") Integer id) {
+        Map<String, Object> product = (Map<String, Object>) wooCommerce.get(EndpointBaseType.PRODUCTS.getValue(), id);
         return product;
     }
 
 
-    @PutMapping("products/{id}")
-    public Map updateProduct(@PathVariable("id") Integer id, Map<String, Object> productInfo) {
-        Map product = wooCommerce.update(EndpointBaseType.PRODUCTS.getValue(), 10, productInfo);
+    @PutMapping("/products/{id}")
+    public Map<String, Object> updateProduct(@PathVariable("id") Integer id, Map<String, Object> productInfo) {
+        Map<String, Object> product = (Map<String, Object>) wooCommerce.update(EndpointBaseType.PRODUCTS.getValue(), 10, productInfo);
         return product;
     }
 
-    @DeleteMapping("products/{id}")
-    public Map deleteProduct(@PathVariable("id") Integer id) {
-        Map product = wooCommerce.delete(EndpointBaseType.PRODUCTS.getValue(), id);
+    @DeleteMapping("/products/{id}")
+    public Map<String, Object> deleteProduct(@PathVariable("id") Integer id) {
+        Map<String, Object> product = (Map<String, Object>) wooCommerce.delete(EndpointBaseType.PRODUCTS.getValue(), id);
         return product;
     }
 
-    @PatchMapping("products/{id}")
-    public Map batchVariation(@PathVariable("id") Integer id, List<Map<String, Object>> variations) {
+    @PatchMapping("/products/{id}")
+    public Map<String, Object> batchVariation(@PathVariable("id") Integer id, List<Map<String, Object>> variations) {
         Map<String, Object> reqOptions = new HashMap<>();
         reqOptions.put("update", variations);
-        Map response = wooCommerce.batch(String.format("products/%d/variations", id), reqOptions);
+        Map<String, Object> response = (Map<String, Object>) wooCommerce.batch(String.format("products/%d/variations", id), reqOptions);
         return response;
     }
 
-    @PatchMapping("products/{id}/batch")
-    public Map batchProduct(@PathVariable("id") Integer id, @RequestParam("name") String name) {
+    @PatchMapping("/products/{id}/batch")
+    public Map<String, Object> batchProduct(@PathVariable("id") Integer id, @RequestParam("name") String name) {
         List<Map<String, Object>> products = new ArrayList<>();
         Map<String, Object> product = new HashMap<>();
         product.put("id", id);
@@ -72,7 +66,7 @@ public class WooCommerceController {
         products.add(product);
         Map<String, Object> reqOptions = new HashMap<>();
         reqOptions.put("update", products);
-        Map response = wooCommerce.batch(EndpointBaseType.PRODUCTS.getValue(), reqOptions);
+        Map<String, Object> response = (Map<String, Object>) wooCommerce.batch(EndpointBaseType.PRODUCTS.getValue(), reqOptions);
         return response;
     }
 
--- service-fooda/src/main/java/org/sibernetik/fooda/service/WooConfig.java
+++ service-fooda/src/main/java/org/sibernetik/fooda/service/WooConfig.java
@@ -1,10 +1,18 @@
 package org.sibernetik.fooda.service;
 
+import com.icoderman.woocommerce.oauth.*;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.icoderman.woocommerce.*;
 
 @Configuration
 public class WooConfig {
     
+    private final String CONSUMER_KEY = "ck_b969666524ce65b909b7cb9c3b3598f49a613ef8";
+    private final String CONSUMER_SECRET = "cs_bb15fb7a9b1a84d69c1ae864588985aa2df66f68";
+    private final String WC_URL = "http://localhost:8888/index.php";
 
     @Bean
     public WooCommerce authenticateWoocommerce(){
